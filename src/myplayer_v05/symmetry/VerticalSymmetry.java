91ba0de9b10df7ca25389f7415b7d43a353ab817
Jasper van Merle <jaspervmerle@gmail.com>
2023-09-23 16:57:51 +0200

    v05

---
+++ src/myplayer_v05/symmetry/VerticalSymmetry.java
@@ -0,0 +1,10 @@
+package myplayer_v05.symmetry;
+
+import aic2023.user.Location;
+import myplayer_v05.util.SharedArray;
+
+public class VerticalSymmetry extends Symmetry {
+    public VerticalSymmetry(SharedArray sharedArray) {
+        super(sharedArray, true, false);
+    }
+}

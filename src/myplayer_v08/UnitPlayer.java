b978b099081a8cfd9cbef5def411b4239ea90ec0
Jasper van Merle <jaspervmerle@gmail.com>
2023-09-23 17:48:44 +0200

    v08

---
+++ src/myplayer_v08/UnitPlayer.java
@@ -0,0 +1,69 @@
+package myplayer_v08;
+
+import aic2023.user.UnitController;
+import aic2023.user.UnitType;
+import myplayer_v08.unit.Batter;
+import myplayer_v08.unit.Catcher;
+import myplayer_v08.unit.HQ;
+import myplayer_v08.unit.Pitcher;
+import myplayer_v08.unit.Unit;
+
+public class UnitPlayer {
+    public void run(UnitController uc) {
+        Unit unit = createUnit(uc);
+
+        if (unit == null) {
+            return;
+        }
+
+        while (true) {
+            if (performTurn(uc, unit)) {
+                uc.yield();
+            }
+        }
+    }
+
+    private boolean performTurn(UnitController uc, Unit unit) {
+        int startRound = uc.getRound();
+        int startBytecodes = uc.getEnergyUsed();
+
+        try {
+            unit.run();
+        } catch (Exception e) {
+            uc.println("Exception in unit #" + uc.getInfo().getID() + " (" + uc.getType() + ") in round " + uc.getRound() + ": " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        int endRound = uc.getRound();
+        int endBytecodes = uc.getEnergyUsed();
+        int maxBytecodes = 15000;
+
+        int usedBytecodes = startRound == endRound
+            ? endBytecodes - startBytecodes
+            : (maxBytecodes - startBytecodes) + Math.max(0, endRound - startRound - 1) * maxBytecodes + endBytecodes;
+
+        double bytecodePercentage = (double) usedBytecodes / (double) maxBytecodes * 100.0;
+        if (bytecodePercentage > 95) {
+            uc.println("High bytecode usage in unit #" + uc.getInfo().getID() + " (" + uc.getType() + ") in round " + uc.getRound() + ": " + usedBytecodes + " (" + bytecodePercentage + "%)");
+        }
+
+        return bytecodePercentage < 100;
+    }
+
+    private Unit createUnit(UnitController uc) {
+        UnitType type = uc.getType();
+
+        if (type == UnitType.HQ) {
+            return new HQ(uc);
+        } else if (type == UnitType.PITCHER) {
+            return new Pitcher(uc);
+        } else if (type == UnitType.BATTER) {
+            return new Batter(uc);
+        } else if (type == UnitType.CATCHER) {
+            return new Catcher(uc);
+        }
+
+        uc.println("Unknown unit type: " + type);
+        return null;
+    }
+}
---
+++ src/myplayer_v08/util/ExploredTiles.java
@@ -0,0 +1,43 @@
+package myplayer_v08.util;
+
+import aic2023.user.Location;
+
+public class ExploredTiles {
+    public long[] rows;
+
+    private int xOffset;
+    private boolean xOffsetSubtract;
+
+    private int yOffset;
+    private boolean yOffsetSubtract;
+
+    public ExploredTiles(long[] rows, int xOffset, boolean xOffsetSubtract, int yOffset, boolean yOffsetSubtract) {
+        this.rows = rows;
+        this.xOffset = xOffset;
+        this.xOffsetSubtract = xOffsetSubtract;
+        this.yOffset = yOffset;
+        this.yOffsetSubtract = yOffsetSubtract;
+    }
+
+    public boolean isExplored(Location location) {
+        int x = xOffsetSubtract ? xOffset - location.x : location.x - xOffset;
+        int y = yOffsetSubtract ? yOffset - location.y : location.y - yOffset;
+        return (rows[y] & (1L << x)) != 0;
+    }
+
+    public void setExplored(Location location) {
+        int x = xOffsetSubtract ? xOffset - location.x : location.x - xOffset;
+        int y = yOffsetSubtract ? yOffset - location.y : location.y - yOffset;
+        rows[y] |= 1L << x;
+    }
+
+    public int countExplored() {
+        int count = 0;
+
+        for (long row : rows) {
+            count += Long.bitCount(row);
+        }
+
+        return count;
+    }
+}

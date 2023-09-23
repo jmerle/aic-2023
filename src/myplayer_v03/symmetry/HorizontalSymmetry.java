e40d7009a60c58dfb830a8415a093cce491c717c
Jasper van Merle <jaspervmerle@gmail.com>
2023-09-23 15:43:42 +0200

    v03

---
+++ src/myplayer_v03/symmetry/HorizontalSymmetry.java
@@ -0,0 +1,9 @@
+package myplayer_v03.symmetry;
+
+import myplayer_v03.util.SharedArray;
+
+public class HorizontalSymmetry extends Symmetry {
+    public HorizontalSymmetry(SharedArray sharedArray) {
+        super(sharedArray, false, true);
+    }
+}
---
+++ src/myplayer_v03/unit/Batter.java
@@ -0,0 +1,104 @@
+package myplayer_v03.unit;
+
+import aic2023.user.Direction;
+import aic2023.user.Location;
+import aic2023.user.UnitController;
+import aic2023.user.UnitInfo;
+import aic2023.user.UnitType;
+import myplayer_v03.util.ExploredObject;
+
+public class Batter extends MoveableUnit {
+    public Batter(UnitController uc) {
+        super(uc, UnitType.BATTER);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        if (uc.canMove()) {
+            Location moveTarget = getMoveTarget();
+            if (moveTarget == null) {
+                moveTarget = sharedArray.getOpponentHQ();
+            }
+
+            if (moveTarget != null) {
+                tryMoveTo(moveTarget);
+            }
+        }
+
+        if (uc.canAct()) {
+            Location myLocation = uc.getLocation();
+
+            for (Direction direction : adjacentDirections) {
+                boolean batted = false;
+
+                for (int i = 1; i <= 3; i++) {
+                    Location location = myLocation.add(direction);
+                    if (!uc.canSenseLocation(location)) {
+                        break;
+                    }
+
+                    UnitInfo unit = uc.senseUnitAtLocation(myLocation.add(direction));
+                    if (unit == null) {
+                        continue;
+                    }
+
+                    if (unit.getTeam() == opponentTeam && unit.getType() != UnitType.HQ) {
+                        batted = tryBat(direction, i);
+                    }
+
+                    break;
+                }
+
+                if (batted) {
+                    break;
+                }
+            }
+        }
+
+        explore();
+    }
+
+    private Location getMoveTarget() {
+        Location bestLocation = null;
+        int minDistance = Integer.MAX_VALUE;
+
+        Location myLocation = uc.getLocation();
+
+        for (ExploredObject object : sharedArray.getExploredBases()) {
+            if (object.occupation != sharedArray.OCCUPATION_OPPONENT) {
+                continue;
+            }
+
+            int distance = myLocation.distanceSquared(object.location);
+            if (distance < minDistance) {
+                bestLocation = object.location;
+                minDistance = distance;
+            }
+        }
+
+        for (ExploredObject object : sharedArray.getExploredStadiums()) {
+            if (object.occupation != sharedArray.OCCUPATION_OPPONENT) {
+                continue;
+            }
+
+            int distance = myLocation.distanceSquared(object.location);
+            if (distance < minDistance) {
+                bestLocation = object.location;
+                minDistance = distance;
+            }
+        }
+
+        return bestLocation;
+    }
+
+    private boolean tryBat(Direction direction, int distance) {
+        if (uc.canBat(direction, distance)) {
+            uc.bat(direction, distance);
+            return true;
+        }
+
+        return false;
+    }
+}

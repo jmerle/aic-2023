468dc3f35c40c1b5f00fe37ea115002b487d77c7
Jasper van Merle <jaspervmerle@gmail.com>
2023-09-29 01:02:13 +0200

    v20

---
+++ src/myplayer_v20/symmetry/HorizontalSymmetry.java
@@ -0,0 +1,9 @@
+package myplayer_v20.symmetry;
+
+import myplayer_v20.util.SharedArray;
+
+public class HorizontalSymmetry extends Symmetry {
+    public HorizontalSymmetry(SharedArray sharedArray) {
+        super(sharedArray, false, true);
+    }
+}
---
+++ src/myplayer_v20/symmetry/RotationalSymmetry.java
@@ -0,0 +1,9 @@
+package myplayer_v20.symmetry;
+
+import myplayer_v20.util.SharedArray;
+
+public class RotationalSymmetry extends Symmetry {
+    public RotationalSymmetry(SharedArray sharedArray) {
+        super(sharedArray, true, true);
+    }
+}

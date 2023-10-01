a0caeee5743e2277e9aee641fc46357268b1e15f
Jasper van Merle <jaspervmerle@gmail.com>
2023-10-01 02:07:20 +0200

    v22

---
+++ src/myplayer_v22/symmetry/HorizontalSymmetry.java
@@ -0,0 +1,9 @@
+package myplayer_v22.symmetry;
+
+import myplayer_v22.util.SharedArray;
+
+public class HorizontalSymmetry extends Symmetry {
+    public HorizontalSymmetry(SharedArray sharedArray) {
+        super(sharedArray, false, true);
+    }
+}
